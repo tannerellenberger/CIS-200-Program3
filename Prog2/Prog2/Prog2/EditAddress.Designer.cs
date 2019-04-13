9d279936a9de485ef2d3ca86597a2cf8f7a28f32
Tanner Ellenberger <49576287+tannerellenberger@users.noreply.github.com>
2019-04-12 23:54:41 -0400

    Add files via upload

---
+++ Prog2/Prog2/Prog2/EditAddress.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Prog2
+{
+    partial class EditAddress
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addressCombo = new System.Windows.Forms.ComboBox();
+            this.addressLabel = new System.Windows.Forms.Label();
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // addressCombo
+            // 
+            this.addressCombo.FormattingEnabled = true;
+            this.addressCombo.Location = new System.Drawing.Point(60, 53);
+            this.addressCombo.Name = "addressCombo";
+            this.addressCombo.Size = new System.Drawing.Size(224, 21);
+            this.addressCombo.TabIndex = 0;
+            // 
+            // addressLabel
+            // 
+            this.addressLabel.AutoSize = true;
+            this.addressLabel.Location = new System.Drawing.Point(60, 34);
+            this.addressLabel.Name = "addressLabel";
+            this.addressLabel.Size = new System.Drawing.Size(78, 13);
+            this.addressLabel.TabIndex = 1;
+            this.addressLabel.Text = "Select Address";
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(39, 219);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 2;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(209, 219);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 3;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // EditAddress
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 293);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.addressLabel);
+            this.Controls.Add(this.addressCombo);
+            this.Name = "EditAddress";
+            this.Text = "EditAddress";
+            this.Load += new System.EventHandler(this.EditAddress_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox addressCombo;
+        private System.Windows.Forms.Label addressLabel;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
+    }
+}
---
+++ Prog2/Prog2/Prog2/EditAddress.cs
@@ -0,0 +1,55 @@
+﻿// D3894
+// Program 3
+// Due 11/12/18
+// CIS 200-01
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prog2
+{
+    public partial class EditAddress : Form
+    {
+
+       
+
+        public EditAddress(List<Address>Addresses)
+        {
+            InitializeComponent();
+            addressList = Addresses;
+        }
+
+        List<Address> addressList;
+
+        private void EditAddress_Load(object sender, EventArgs e)
+        {
+            foreach (Address a in addressList)
+            {
+                addressCombo.Items.Add(a.Name); //Displays names on combo box
+            }
+        }
+
+        //Pre: User clicks ok button
+        //Post: the form runs or closes depending on errors
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            if (ValidateChildren())
+                this.DialogResult = DialogResult.OK;
+        }
+
+        //Pre: User clicks cancel button
+        //Post: Form closes
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+             
+                this.DialogResult = DialogResult.Cancel;
+        }
+    }
+}
